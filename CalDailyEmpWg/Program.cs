a83ec436162b0096c5e161bbbc6416b51b306953
Radhika <reddypallyradhika@gmail.com>
2022-07-18 22:18:43 +0530

    Employee Wage Uc 2

---
+++ CalDailyEmpWg/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        //variables
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        //computation
+        int empCheck = random.Next(0,2);
+        if  (empCheck == IS_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("EmpWage:" -+ empWage);
+    }
+}
